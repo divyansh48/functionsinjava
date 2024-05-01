3bb666aa96a5ee33f1e4ff52ea5725df70249b53
divyansh48 <166692812+divyansh48@users.noreply.github.com>
2024-05-01 16:05:39 +0530

    Add files via upload

---
+++ parameters.java
@@ -0,0 +1,18 @@
+package com.divyansh;
+
+import java.util.Scanner;
+
+public class parameters {
+
+    public static void main(String[] args) {
+        int ans = sum1(20,30);
+        System.out.println(ans);
+    }
+
+    static int sum1(int a, int b){
+        int sum = a + b;
+
+        return sum;
+
+    }
+}
